faf447a6189dd98ffcedfa1a1c5b0dd909958505
Mykolas Raudžius <raudziusmykolas@gmail.com>
2022-06-16 19:36:16 +0300

    feat: generate random collor factory

---
+++ app.js
@@ -0,0 +1,7 @@
+function generateRandomColor() {
+  const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+  const r = randomBetween(0, 255);
+  const g = randomBetween(0, 255);
+  const b = randomBetween(0, 255);
+  return `rgb(${r},${g},${b})`;
+}
